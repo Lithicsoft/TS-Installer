2bdd9654e042d055c80c5778878c77ce8d0458f3
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-07-06 06:49:12 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Lithicsoft_Trainer_Studio_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            progressBar1 = new ProgressBar();
+            label3 = new Label();
+            richTextBox1 = new RichTextBox();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Light", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(218, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(482, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Lithicsoft Trainer Studio Installer";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Traner_Studio;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 200);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Light", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Highlight;
+            label2.Location = new Point(218, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(380, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Copyright © 2024 Lithicsoft Organization.";
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(713, 425);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Enabled = false;
+            button3.ForeColor = SystemColors.Highlight;
+            button3.Location = new Point(580, 425);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Install Trainer Studio";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 425);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(562, 23);
+            progressBar1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Highlight;
+            label3.Location = new Point(218, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(16, 15);
+            label3.TabIndex = 8;
+            label3.Text = "...";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 218);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(776, 201);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.Highlight;
+            label4.Location = new Point(717, 197);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Change Log";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(800, 460);
+            Controls.Add(label4);
+            Controls.Add(richTextBox1);
+            Controls.Add(label3);
+            Controls.Add(progressBar1);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button button1;
+        private Button button3;
+        private ProgressBar progressBar1;
+        private Label label3;
+        private RichTextBox richTextBox1;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+// License: Apache-2.0
+/*
+ * Form1.cs: Main form for launcher, installer, updater for Trainer Studio
+ *
+ * (C) Copyright 2024 Lithicsoft Organization
+ * Author: Bui Nguyen Tan Sang <tansangbuinguyen52@gmail.com>
+ */
+
+using IWshRuntimeLibrary;
+using System.IO.Compression;
+using System.Net;
+using File = System.IO.File;
+
+namespace Lithicsoft_Trainer_Studio_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists("build.txt") || Directory.Exists("Lithicsoft Trainer Studio"))
+            {
+                button3.Enabled = false;
+                button1.Enabled = true;
+
+                label3.Text = "Build: " + File.ReadAllText("build.txt");
+            }
+            else
+            {
+                button3.Enabled = true;
+                button1.Enabled = false;
+
+                label3.Text = "Ready for install";
+            }
+
+            try
+            {
+                string url = "https://raw.githubusercontent.com/EndermanPC/test/main/changelog.txt";
+
+                using (var webClient = new WebClient())
+                {
+                    richTextBox1.Text = webClient.DownloadString(url);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error getting change log: {ex.Message}", "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForUpdates();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            await UpdateTrainerStudio();
+            button1.Enabled = true;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            await InstallTrainerStudio();
+        }
+
+        private void CheckForUpdates()
+        {
+            try
+            {
+                string url = "https://raw.githubusercontent.com/EndermanPC/test/main/update.txt";
+                string localFilePath = "build.txt";
+
+                using (var webClient = new WebClient())
+                {
+                    string fileContents = webClient.DownloadString(url);
+                    string[] lines = fileContents.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (lines.Length > 0)
+                    {
+                        string firstLine = lines[0];
+
+                        if (File.Exists(localFilePath))
+                        {
+                            string localFileText = File.ReadAllText(localFilePath);
+
+                            if (firstLine != localFileText)
+                            {
+                                MessageBox.Show("An update is available. Please update the new version.", "Update Available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("An update is available. Please update the new version.", "Update Available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error checking for updates: {ex.Message}", "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task UpdateTrainerStudio()
+        {
+            try
+            {
+                var progress = new Progress<int>(value => progressBar1.Value = value);
+                await PerformUpdateOrInstall("update", progress);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating Lithicsoft Trainer Studio: {ex.Message}", "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task InstallTrainerStudio()
+        {
+            try
+            {
+                var progress = new Progress<int>(value => progressBar1.Value = value);
+                await PerformUpdateOrInstall("install", progress);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error installing Lithicsoft Trainer Studio: {ex.Message}", "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task PerformUpdateOrInstall(string mode, IProgress<int> progress)
+        {
+            progress.Report(0);
+            string url = "https://raw.githubusercontent.com/EndermanPC/test/main/update.txt";
+            string localFilePath = "build.txt";
+            string destinationFolder = "Lithicsoft Trainer Studio";
+
+            using (var webClient = new WebClient())
+            {
+                string fileContents = await webClient.DownloadStringTaskAsync(url);
+                string[] lines = fileContents.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (lines.Length > 0)
+                {
+                    string firstLine = lines[0];
+                    string secondLine = lines.Length > 1 ? lines[1] : string.Empty;
+
+                    File.WriteAllText(localFilePath, firstLine);
+
+                    string fileToDownload = secondLine;
+                    string zipFilePath = "downloaded.zip";
+
+                    progress.Report(20);
+                    await webClient.DownloadFileTaskAsync(new Uri(fileToDownload), zipFilePath);
+                    progress.Report(50);
+                    ZipFile.ExtractToDirectory(zipFilePath, destinationFolder);
+                    progress.Report(90);
+
+                    File.Delete(zipFilePath);
+                    progress.Report(100);
+
+                    if (mode == "install")
+                    {
+                        CreateShortcut("Lithicsoft Trainer Studio", Path.GetFullPath(Path.Combine(destinationFolder, "Lithicsoft Trainer Studio.exe")));
+                        MessageBox.Show("Lithicsoft Trainer Studio has been installed!", "Installer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        button3.Enabled = false;
+                        button1.Enabled = true;
+                    }
+                    else if (mode == "update")
+                    {
+                        MessageBox.Show("Lithicsoft Trainer Studio has been updated!", "Installer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    label3.Text = "Build: " + File.ReadAllText(localFilePath);
+                }
+            }
+        }
+
+        private void CreateShortcut(string shortcutName, string targetFileLocation)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string startMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+
+            string desktopShortcutLocation = Path.Combine(desktopPath, $"{shortcutName}.lnk");
+            string startMenuShortcutLocation = Path.Combine(startMenuPath, $"{shortcutName}.lnk");
+
+            WshShell shell = new WshShell();
+
+            IWshShortcut desktopShortcut = (IWshShortcut)shell.CreateShortcut(desktopShortcutLocation);
+            desktopShortcut.Description = "Shortcut for Lithicsoft Trainer Studio";
+            desktopShortcut.TargetPath = targetFileLocation;
+            desktopShortcut.Save();
+
+            IWshShortcut startMenuShortcut = (IWshShortcut)shell.CreateShortcut(startMenuShortcutLocation);
+            startMenuShortcut.Description = "Shortcut for Lithicsoft Trainer Studio";
+            startMenuShortcut.TargetPath = targetFileLocation;
+            startMenuShortcut.Save();
+        }
+    }
+}
